86b6c4c723979eff41295692d88a49636da00f38
Baspar <me@bastien-laine.io>
2019-06-16 20:26:40 +0800

    [edit] Edit baspar keymap

--- keyboards/dz60/keymaps/baspar/keymap.c
+++ keyboards/dz60/keymaps/baspar/keymap.c
@@ -4,7 +4,6 @@
 #define _FUNCTION 1
 
 #define ______ KC_TRNS
-
 enum custom_keycodes {
   A_ALT = SAFE_RANGE,
   E_ALT = SAFE_RANGE,
@@ -14,18 +13,21 @@ enum custom_keycodes {
   C_ALT = SAFE_RANGE,
 };
 
-
 bool process_alt_key_in_progress = false;
+/* uint16_t index = 0; */
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case MO(_FUNCTION):
+            /* SEND_STRING(SS_TAP(RGB_TOG)); */
             if (!record->event.pressed) {
                 process_alt_key_in_progress = false;
             } else {
-                process_alt_key_in_progress = false;
+                process_alt_key_in_progress = true;
             }
             break;
+            /* case (E_ALT) { */
+            /* } */
     }
     return true;
 }
@@ -42,21 +44,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------------------------------------------------------------------+
      * |    Shift        |   Z   |   X   |   C   |   V   |   B   |   N   |   M   |   ,   |   .   |   /   |    Shift    |  Fn   |
      * |-----------------------------------------------------------------------------------------------------------------------+
-     * |   Ctrl  |   Alt   |   Cmd   |                     Space                       |  Alt  |   L   |   D   |   U   |   R   |
+     * |   Ctrl  |   GUI   |   Alt   |                     Space                       |  Alt  |   L   |   D   |   U   |   R   |
      * `-----------------------------------------------------------------------------------------------------------------------'
      */
 
     [_DEFAULT] = LAYOUT_60_split_rshift_5x1u(
-            KC_GRAVE , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL        , KC_BSPC,
-            KC_TAB   , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC       , KC_BSLS,
-            KC_ESCAPE, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT        ,
-            KC_LSFT  , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, MO(_FUNCTION) ,
-            KC_LCTL  , KC_LALT, KC_LGUI, KC_SPC , KC_RALT, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT
+            KC_GRAVE         , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7 , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL        , KC_BSPC,
+            KC_TAB           , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC       , KC_BSLS,
+            LCTL_T(KC_ESCAPE), KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT        ,
+            KC_LSFT          , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, MO(_FUNCTION) ,
+            KC_LCTL          , KC_LGUI, KC_LALT, KC_SPC , KC_RALT, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
             ),
 
     /* Function
      * ,-----------------------------------------------------------------------------------------------------------------------.
-     * |       |   F1  |   F2  |   F3  |   F4  |   F5  |   F6  |   F7  |   F8  |   F9  |   F0  |  F11  |  F12  |               |
+     * | Reset |   F1  |   F2  |   F3  |   F4  |   F5  |   F6  |   F7  |   F8  |   F9  |  F10  |  F11  |  F12  |               |
      * |-----------------------------------------------------------------------------------------------------------------------+
      * |           |       |       | E_ALT |       |       |       | U_ALT | I_ALT | O_ALT |       |       |       |           |
      * |-----------------------------------------------------------------------------------------------------------------------+
@@ -64,15 +66,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------------------------------------------------------------------+
      * |                 |       |       | C_ALT |       |       |       |       |       |       |       |             |       |
      * |-----------------------------------------------------------------------------------------------------------------------+
-     * |         |         |         |                                                 |       |       |       |       |       |
+     * |         |         |         |                    VOL_TOGGLE                   |       |       | VOL_D | VOL_U |       |
      * `-----------------------------------------------------------------------------------------------------------------------'
      */
 
     [_FUNCTION] = LAYOUT_60_split_rshift_5x1u(
-            ______   , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12        , ______ ,
-            ______   , ______ , ______ , E_ALT  , ______ , ______ , ______ , U_ALT  , I_ALT  , O_ALT  , ______ , ______ , ______        , ______ ,
-            ______   , A_ALT  , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______        ,
-            ______   , ______ , ______ , C_ALT  , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______        ,
-            ______   , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______
+            RESET  , KC_F1  , KC_F2  , KC_F3         , KC_F4  , KC_F5  , KC_F6            , KC_F7          , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , ______ ,
+            ______ , ______ , ______ , E_ALT         , ______ , ______ , ______           , U_ALT          , I_ALT  , O_ALT  , ______ , ______ , ______ , ______ ,
+            ______ , A_ALT  , ______ , ______        , ______ , ______ , ______           , ______         , ______ , ______ , ______ , ______ , ______ ,
+            ______ , ______ , ______ , C_ALT         , ______ , ______ , ______           , ______         , ______ , ______ , ______ , ______ , ______ ,
+            ______ , ______ , ______ , KC_AUDIO_MUTE , ______ , ______ , KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, ______
             )
 };
