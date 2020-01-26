4fe3512583e479250be4c270ec0a383d316ea86b
Baspar <me@bastien-laine.io>
2020-01-26 13:35:19 +0800

    [add] Baspar: Lowercase accent

--- keyboards/dz60/keymaps/baspar/keymap.c
+++ keyboards/dz60/keymaps/baspar/keymap.c
@@ -18,13 +18,18 @@ user_config_t user_config;
 // Custom keycodes
 #define ______ KC_TRNS
 enum custom_keycodes {
-  CHANGE_OS,
-  A_ALT,
-  E_ALT,
-  I_ALT,
-  O_ALT,
-  U_ALT,
-  C_ALT,
+  CHANGE_OS = SAFE_RANGE,
+  REG_A,
+  REG_E,
+  REG_I,
+  REG_O,
+  REG_U,
+  REG_C,
+  REG_GRV,
+  REG_ACU,
+  REG_CED,
+  REG_TRM,
+  REG_CIR,
 };
 
 void go_to_mode(void) {
@@ -45,13 +50,54 @@ void keyboard_post_init_user(void) {
   go_to_mode();
 }
 
-char* ALT_LETTER = NULL;
-int* ALT_MOD = NULL;
+char ALT_LETTER = '\0';
+int ALT_MOD = -1;
 
 /* send_unicode_hex_string("00E9"); */
 
 void check_accent(void) {
-  if (ALT_LETTER != NULL && ALT_MOD != NULL) {
+  if (ALT_LETTER != '\0' && ALT_MOD != -1) {
+    switch (ALT_LETTER) {
+      case 'a':
+        switch (ALT_MOD) {
+          case REG_CIR: send_unicode_hex_string("00E2"); break;
+          case REG_GRV: send_unicode_hex_string("00E0"); break;
+        }
+        break;
+      case 'e':
+        switch (ALT_MOD) {
+          case REG_ACU: send_unicode_hex_string("00E9"); break;
+          case REG_CIR: send_unicode_hex_string("00EA"); break;
+          case REG_GRV: send_unicode_hex_string("00E8"); break;
+          case REG_TRM: send_unicode_hex_string("00EB"); break;
+        }
+        break;
+      case 'i':
+        switch (ALT_MOD) {
+          case REG_CIR: send_unicode_hex_string("00EE"); break;
+          case REG_TRM: send_unicode_hex_string("00EF"); break;
+        }
+        break;
+      case 'o':
+        switch (ALT_MOD) {
+          case REG_CIR: send_unicode_hex_string("00F4"); break;
+        }
+        break;
+      case 'u':
+        switch (ALT_MOD) {
+          case REG_CIR: send_unicode_hex_string("00FB"); break;
+          case REG_GRV: send_unicode_hex_string("00F9"); break;
+          case REG_TRM: send_unicode_hex_string("00FC"); break;
+        }
+        break;
+      case 'c':
+        switch (ALT_MOD) {
+          case REG_CED: send_unicode_hex_string("00E7"); break;
+        }
+        break;
+    }
+    ALT_LETTER = '\0';
+    ALT_MOD = -1;
   }
 }
 void change_OS (void) {
@@ -61,7 +107,74 @@ void change_OS (void) {
   eeconfig_update_user(user_config.raw);
 }
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  bool shift_pressed = (keyboard_report->mods & MOD_BIT (KC_LSFT)) || (keyboard_report->mods & MOD_BIT (KC_RSFT));
   switch (keycode) {
+    case REG_GRV:
+      if(record->event.pressed) {
+        ALT_MOD = REG_GRV;
+        check_accent();
+      }
+      return false;
+    case REG_ACU:
+      if(record->event.pressed) {
+        ALT_MOD = REG_ACU;
+        check_accent();
+      }
+      return false;
+    case REG_CED:
+      if(record->event.pressed) {
+        ALT_MOD = shift_pressed ? REG_CIR : REG_CED;
+        check_accent();
+      }
+      return false;
+    case REG_TRM:
+      if(record->event.pressed) {
+        ALT_MOD = REG_TRM;
+        check_accent();
+      }
+      return false;
+    case REG_CIR:
+      if(record->event.pressed) {
+        ALT_MOD = REG_CIR;
+        check_accent();
+      }
+      return false;
+    case REG_A:
+      if (record->event.pressed) {
+        ALT_LETTER = shift_pressed ? 'A' : 'a';
+        check_accent();
+      }
+      return false;
+    case REG_E:
+      if (record->event.pressed) {
+        ALT_LETTER = shift_pressed ? 'E' : 'e';
+        check_accent();
+      }
+      return false;
+    case REG_I:
+      if (record->event.pressed) {
+        ALT_LETTER = shift_pressed ? 'I' : 'i';
+        check_accent();
+      }
+      return false;
+    case REG_O:
+      if (record->event.pressed) {
+        ALT_LETTER = shift_pressed ? 'O' : 'o';
+        check_accent();
+      }
+      return false;
+    case REG_U:
+      if (record->event.pressed) {
+        ALT_LETTER = shift_pressed ? 'U' : 'u';
+        check_accent();
+      }
+      return false;
+    case REG_C:
+      if (record->event.pressed) {
+        ALT_LETTER = shift_pressed ? 'C' : 'c';
+        check_accent();
+      }
+      return false;
     case CHANGE_OS:
       if (!record->event.pressed) {
         change_OS();
@@ -119,45 +232,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Function
    * ,-----------------------------------------------------------------------------------------------------------------------.
-   * |       |   F1  |   F2  |   F3  |   F4  |   F5  |   F6  |   F7  |   F8  |   F9  |  F10  |  F11  |  F12  |    Reset      |
+   * |REG_GRV|   F1  |   F2  |   F3  |   F4  |   F5  |   F6  |   F7  |   F8  |   F9  |  F10  |  F11  |  F12  |    Reset      |
    * |-----------------------------------------------------------------------------------------------------------------------+
-   * |           |       |       |       |       |       |       |       |       |       |       |       |       |           |
+   * |           |       |       | REG_E |       |       |       | REG_U | REG_I | REG_O |       |       |       |           |
    * |-----------------------------------------------------------------------------------------------------------------------+
-   * |             |       |       |       |       |       |       |       |       |       |       |       |                 |
+   * |             | REG_A |       |       |       |       |       |       |       |       |REG_TRM|REG_ACU|                 |
    * |-----------------------------------------------------------------------------------------------------------------------+
-   * |                 |       |       |       |       |       |       |       |       |       |       |             |       |
+   * |                 |       |       | REG_C |       |       |       |       |REG_CED|REG_CIR|       |             |       |
    * |-----------------------------------------------------------------------------------------------------------------------+
    * |         |CHANGE_OS|         |                    VOL_TOGGLE                   |       |       | VOL_D | VOL_U |       |
    * `-----------------------------------------------------------------------------------------------------------------------'
    */
 
   [_FUNCTION] = LAYOUT_60_split_rshift_5x1u(
-      ______ , KC_F1     , KC_F2  , KC_F3         , KC_F4  , KC_F5  , KC_F6            , KC_F7          , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , RESET  ,
-      ______ , ______    , ______ , E_ALT         , ______ , ______ , ______           , U_ALT          , I_ALT  , O_ALT  , ______ , ______ , ______ , ______ ,
-      ______ , A_ALT     , ______ , ______        , ______ , ______ , ______           , ______         , ______ , ______ , ______ , ______ , ______ ,
-      ______ , ______    , ______ , C_ALT         , ______ , ______ , ______           , ______         , ______ , ______ , ______ , ______ , ______ ,
-      ______ , CHANGE_OS , ______ , KC_AUDIO_MUTE , ______ , ______ , KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, ______
+      REG_GRV , KC_F1     , KC_F2  , KC_F3         , KC_F4  , KC_F5  , KC_F6            , KC_F7          , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12 , RESET  ,
+      ______  , ______    , ______ , REG_E         , ______ , ______ , ______           , REG_U          , REG_I   , REG_O   , ______  , ______  , ______ , ______ ,
+      ______  , REG_A     , ______ , ______        , ______ , ______ , ______           , ______         , ______  , ______  , REG_TRM , REG_ACU , ______ ,
+      ______  , ______    , ______ , REG_C         , ______ , ______ , ______           , ______         , REG_CED , REG_CIR , ______  , ______  , ______ ,
+      ______  , CHANGE_OS , ______ , KC_AUDIO_MUTE , ______ , ______ , KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, ______
       ),
 
   /* Function Mac
    * ,-----------------------------------------------------------------------------------------------------------------------.
-   * |       |   F1  |   F2  |   F3  |   F4  |   F5  |   F6  |   F7  |   F8  |   F9  |  F10  |  F11  |  F12  |    Reset      |
+   * |REG_GRV|   F1  |   F2  |   F3  |   F4  |   F5  |   F6  |   F7  |   F8  |   F9  |  F10  |  F11  |  F12  |    Reset      |
    * |-----------------------------------------------------------------------------------------------------------------------+
-   * |           |       |       | E_ALT |       |       |       | U_ALT | I_ALT | O_ALT |       |       |       |           |
+   * |           |       |       | REG_E |       |       |       | REG_U | REG_I | REG_O |       |       |       |           |
    * |-----------------------------------------------------------------------------------------------------------------------+
-   * |             | A_ALT |       |       |       |       |       |       |       |       |       |       |                 |
+   * |             | REG_A |       |       |       |       |       |       |       |       |REG_TRM|REG_ACU|                 |
    * |-----------------------------------------------------------------------------------------------------------------------+
-   * |                 |       |       | C_ALT |       |       |       |       |       |       |       |             |       |
+   * |                 |       |       | REG_C |       |       |       |       |REG_CED|REG_CIR|       |             |       |
    * |-----------------------------------------------------------------------------------------------------------------------+
    * |         |CHANGE_OS|         |                    VOL_TOGGLE                   |       |       | VOL_D | VOL_U |       |
    * `-----------------------------------------------------------------------------------------------------------------------'
    */
 
   [_FUNCTION_MAC] = LAYOUT_60_split_rshift_5x1u(
-      ______ , KC_F1     , KC_F2  , KC_F3    , KC_F4  , KC_F5  , KC_F6      , KC_F7    , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , RESET  ,
-      ______ , ______    , ______ , E_ALT    , ______ , ______ , ______     , U_ALT    , I_ALT  , O_ALT  , ______ , ______ , ______ , ______ ,
-      ______ , A_ALT     , ______ , ______   , ______ , ______ , ______     , ______   , ______ , ______ , ______ , ______ , ______ ,
-      ______ , ______    , ______ , C_ALT    , ______ , ______ , ______     , ______   , ______ , ______ , ______ , ______ , ______ ,
-      ______ , CHANGE_OS , ______ , KC__MUTE , ______ , ______ , KC__VOLDOWN, KC__VOLUP, ______
+      REG_GRV , KC_F1     , KC_F2  , KC_F3    , KC_F4  , KC_F5  , KC_F6      , KC_F7    , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12 , RESET  ,
+      ______  , ______    , ______ , REG_E    , ______ , ______ , ______     , REG_U    , REG_I   , REG_O   , ______  , ______  , ______ , ______ ,
+      ______  , REG_A     , ______ , ______   , ______ , ______ , ______     , ______   , ______  , ______  , REG_TRM , REG_ACU , ______ ,
+      ______  , ______    , ______ , REG_C    , ______ , ______ , ______     , ______   , REG_CED , REG_CIR , ______  , ______  , ______ ,
+      ______  , CHANGE_OS , ______ , KC__MUTE , ______ , ______ , KC__VOLDOWN, KC__VOLUP, ______
       )
 };
