11c049d4e97686510505054ac076126426ab4ea4
Bastien Laine <me@bastien-laine.io>
2020-03-13 16:24:03 +0800

    [add] RGB_OFF

--- keyboards/dz60/keymaps/baspar/keymap.c
+++ keyboards/dz60/keymaps/baspar/keymap.c
@@ -47,7 +47,8 @@ enum custom_keycodes {
   REG_CIR,
   REG_TIL,
   DEC_COL,
-  INC_COL
+  INC_COL,
+  RGB_OFF
 };
 
 // Accent handling
@@ -141,6 +142,7 @@ void check_accent(void) {
 // Backlight handling
 #define SHIFT_HUE 15
 #define SHIFT_BRIGHTNESS 15
+bool force_rgb_off = false;
 void update_backlight(void) {
   bool use_mac = user_config.use_mac;
   int hue_mac = user_config.hues % 16;
@@ -217,7 +219,15 @@ void keyboard_post_init_user(void) {
 // Main Loop
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   bool shift_pressed = (keyboard_report->mods & MOD_BIT (KC_LSFT)) || (keyboard_report->mods & MOD_BIT (KC_RSFT));
+  if (record->event.pressed && force_rgb_off) {
+    force_rgb_off = false;
+      rgblight_enable_noeeprom();
+  }
   switch (keycode) {
+    case RGB_OFF:
+      rgblight_disable_noeeprom();
+      force_rgb_off = true;
+      return false;
     case DEC_COL:
       if (!record->event.pressed) {
         if (shift_pressed) { change_brightness(-1); } else { change_hue(-1); }
@@ -315,7 +325,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Function
    * ,-----------------------------------------------------------------------------------------------------------------------.
-   * |       |   F1  |   F2  |   F3  |   F4  |   F5  |   F6  |   F7  |   F8  |   F9  |  F10  |  F11  |  F12  |    Reset      |
+   * |RGB_OFF|   F1  |   F2  |   F3  |   F4  |   F5  |   F6  |   F7  |   F8  |   F9  |  F10  |  F11  |  F12  |    Reset      |
    * |-----------------------------------------------------------------------------------------------------------------------+
    * |           |       |       |       |       |       |       |       |       |       |       |DEC_COL|INC_COL|           |
    * |-----------------------------------------------------------------------------------------------------------------------+
@@ -328,7 +338,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [_FUNCTION] = LAYOUT_60_split_rshift_5x1u(
-        ______ ,KC_F1    ,KC_F2 ,KC_F3   ,KC_F4 ,KC_F5  ,KC_F6    ,KC_F7       ,KC_F8     ,KC_F9  ,KC_F10 ,KC_F11  ,KC_F12 ,RESET ,
+        RGB_OFF,KC_F1    ,KC_F2 ,KC_F3   ,KC_F4 ,KC_F5  ,KC_F6    ,KC_F7       ,KC_F8     ,KC_F9  ,KC_F10 ,KC_F11  ,KC_F12 ,RESET ,
         ______ ,______   ,______,______  ,______,______ ,______   ,______      ,______    ,______ ,______ ,DEC_COL ,INC_COL,______,
         ______ ,______   ,______,______  ,______,______ ,______   ,KC__VOLDOWN ,KC__VOLUP ,______ ,______ ,______  ,______ ,
         ______ ,______   ,______,______  ,______,______ ,______   ,______      ,______    ,______ ,______ ,______  ,______ ,
