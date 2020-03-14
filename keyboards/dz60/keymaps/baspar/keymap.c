5f18f9bd86d86c47a4b508bc3ff744acbd5af836
Bastien Laine <me@bastien-laine.io>
2020-03-14 17:35:21 +0800

    [wip] RGB_STEP

--- keyboards/dz60/keymaps/baspar/keymap.c
+++ keyboards/dz60/keymaps/baspar/keymap.c
@@ -48,7 +48,8 @@ enum custom_keycodes {
   REG_TIL,
   DEC_COL,
   INC_COL,
-  RGB_OFF
+  RGB_OFF,
+  RGB_STP
 };
 
 enum rgb_status {
@@ -229,6 +230,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return false;
   }
   switch (keycode) {
+    /* case RGB_STP: */
+    /*   if (!record->event.pressed) { */
+    /*     rgblight_step_noeeprom(); */
+    /*     return false; */
+    /*   } */
     case RGB_OFF:
       if (!record->event.pressed) {
         if (shift_pressed && rgb_status == ON) {
@@ -354,7 +360,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FUNCTION] = LAYOUT_60_split_rshift_5x1u(
         RGB_OFF,KC_F1    ,KC_F2 ,KC_F3   ,KC_F4 ,KC_F5  ,KC_F6    ,KC_F7       ,KC_F8     ,KC_F9  ,KC_F10 ,KC_F11  ,KC_F12 ,RESET ,
-        ______ ,______   ,______,______  ,______,______ ,______   ,______      ,______    ,______ ,______ ,DEC_COL ,INC_COL,______,
+        ______ ,______   ,______,______  ,______,______ ,______   ,______      ,______    ,______ ,______ ,DEC_COL ,INC_COL,RGB_STP,
         ______ ,______   ,______,______  ,______,______ ,______   ,KC__VOLDOWN ,KC__VOLUP ,______ ,______ ,______  ,______ ,
         ______ ,______   ,______,______  ,______,______ ,______   ,______      ,______    ,______ ,______ ,______  ,______ ,
         ______ ,CHANGE_OS,______,KC__MUTE,______,KC_HOME,KC_PGDOWN,KC_PGUP     ,KC_END
